2f65c0439e52d83b8e434d01f60008b7741bdfe9
amonetlol <65382640+amonetlol@users.noreply.github.com>
2025-12-23 15:27:16 -0300

    Update fedora_qtile.sh

--- fedora_qtile.sh
+++ fedora_qtile.sh
@@ -52,8 +52,8 @@ install_firefox_official() {
 
 install_vscode() {
     echo "Instalando Visual Studio Code (oficial Microsoft)..."
-    sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
-    sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
+    sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc &&
+    echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\nautorefresh=1\ntype=rpm-md\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" | sudo tee /etc/yum.repos.d/vscode.repo > /dev/null
     sudo dnf check-update
     sudo dnf install -y code
 }
