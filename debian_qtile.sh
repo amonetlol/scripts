e8a3c349ea6fed2956d07ea2660dad40f479c6a0
amonetlol <65382640+amonetlol@users.noreply.github.com>
2025-12-24 20:24:11 -0300

    Update debian_qtile.sh

--- debian_qtile.sh
+++ debian_qtile.sh
@@ -24,6 +24,11 @@ set -e  # Para o script em caso de erro
 # Funções
 # ==============================================
 
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[1;33m'
+NC='\033[0m'
+
 # ----- GLOBAL ----------
 link() {
     local src="$1"
