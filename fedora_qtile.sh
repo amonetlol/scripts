4f45eb59186d530678ae3cfa64ab95d8b693c137
amonetlol <65382640+amonetlol@users.noreply.github.com>
2025-12-24 20:24:32 -0300

    Update fedora_qtile.sh

--- fedora_qtile.sh
+++ fedora_qtile.sh
@@ -5,6 +5,11 @@ set -e
 # Configurações iniciais
 # ==============================================
 
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[1;33m'
+NC='\033[0m'
+
 # ----- GLOBAL ----------
 link() {
     local src="$1"
