ed525d93b0c1ade923ef9e7d34dde39976ad9140
amonetlol <65382640+amonetlol@users.noreply.github.com>
2025-12-24 21:14:20 -0300

    Create debloat_garuda_i3.sh

---
+++ DEBLOAT/debloat_garuda_i3.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+yay -R garuda-i3-settings i3-wm i3blocks i3lock i3status xarchiver xfce4-terminal tint2 onboard pacseek peek pragha geary geany jgmenu micro firedragon galculator copyq arandr deluge deluge-gtk modem-manager-gui gnome-firmware
