cebb773f26e5aca813ecba71f984d80fe2c5f5da
amonetlol <65382640+amonetlol@users.noreply.github.com>
2026-01-08 15:50:17 -0300

    Create solus_debloat_install.sh

---
+++ TEMP/solus_debloat_install.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+# Debloat:
+sudo eopkg remove gnome-weather gnome-calendar snapshot gnome-software thunderbird libreoffice-calc libreoffice-common libreoffice-draw libreoffice-impress libreoffice-writer decibels
+#
+# Install:
+neovim git kitty xclip wl-clipboard alacritty starship btop eza duf screenfetch ripgrep fd htop screenfetch catfish fastfetch gcc ripgrep lazygit bat jq bc lua51 luarocks tree-sitter nodejs findutils coreutils
