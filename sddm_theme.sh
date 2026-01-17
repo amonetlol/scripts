1da7aaf04dbf7ac45b76fb70d6ce4840fb077f01
amonetlol <65382640+amonetlol@users.noreply.github.com>
2026-01-17 12:59:05 -0300

    Update sddm_theme.sh

--- sddm_theme.sh
+++ sddm_theme.sh
@@ -4,14 +4,25 @@ CURRENT=$(pwd)
 cd /tmp
 
 # Install Catppuccin theme for SDDM
-wget https://github.com/catppuccin/sddm/releases/download/v1.1.2/catppuccin-macchiato-mauve-sddm.zip
+# wget https://github.com/catppuccin/sddm/releases/download/v1.1.2/catppuccin-macchiato-mauve-sddm.zip
 
-if [ -f catppuccin-macchiato-mauve-sddm.zip ]; then
-  7z x catppuccin-macchiato-mauve-sddm.zip
-  sudo cp -r catppuccin-macchiato-mauve /usr/share/sddm/themes/
+# if [ -f catppuccin-macchiato-mauve-sddm.zip ]; then
+#   7z x catppuccin-macchiato-mauve-sddm.zip
+#   sudo cp -r catppuccin-macchiato-mauve /usr/share/sddm/themes/
+#   sudo tee /etc/sddm.conf <<'EOF'
+# [Theme]
+# Current=catppuccin-macchiato-mauve
+# EOF
+# fi
+
+wget https://github.com/catppuccin/sddm/releases/download/v1.1.2/catppuccin-macchiato-blue-sddm.zip
+
+if [ -f catppuccin-macchiato-blue-sddm.zip ]; then
+  7z x catppuccin-macchiato-blue-sddm.zip
+  sudo cp -r catppuccin-macchiato-blue /usr/share/sddm/themes/
   sudo tee /etc/sddm.conf <<'EOF'
 [Theme]
-Current=catppuccin-macchiato-mauve
+Current=catppuccin-macchiato-blue
 EOF
 fi
 
