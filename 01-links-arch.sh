04ef2eb2b4d0619de035e36c743292ca8b9cc07f
amonetlol <65382640+amonetlol@users.noreply.github.com>
2026-06-06 12:07:46 -0300

    Update 01-links-arch.sh

--- 01-links-arch.sh
+++ 01-links-arch.sh
@@ -5,6 +5,7 @@ chmod +x $PWD/bin/*
 ln -sf $PWD/.aliases ~/.aliases
 ln -sf $PWD/.aliases-arch ~/.aliases-arch
 ln -sf $PWD/.bashrc ~/.bashrc
+ln -sf $PWD/.functions ~/.functions
 ln -sf $PWD/config/starship.toml ~/.config/starship.toml
 ln -sf $PWD/config/fastfetch ~/.config/fastfetch
 ln -sf $PWD/config/neofetch ~/.config/neofetch
